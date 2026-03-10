9d62db02af850791598b681b70e1900319fb86af
Mobin Amiri <amirialimobin@gmail.com>
2026-03-10 12:52:41 +0100

    added everything in their own folder and file so its readable

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router';
 import './App.css'
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/Tracking/TrackingPage';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
---
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,42 @@
+import { Header } from '../../components/Header';
+import './OrdersPage.css';
+import axios from 'axios';
+import { useState, useEffect, Fragment } from 'react'
+import { OrderHeader } from './OrderHeader';
+import { OrderDetails } from './OrderDetails';
+
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/orders?expand=products').then((response) => {
+      setOrders(response.data);
+    })
+  }, [])
+
+
+  return (
+    <>
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+
+                <OrderHeader order={order} />
+
+                <OrderDetails order={order} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
