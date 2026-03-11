2d49a30fa002093557d593b1ee28c37009ca95a9
Mobin Amiri <amirialimobin@gmail.com>
2026-03-11 10:59:12 +0100

    added loadcart

--- ecommerce-project/src/pages/home/ProductsGrid.jsx
+++ ecommerce-project/src/pages/home/ProductsGrid.jsx
@@ -1,7 +1,8 @@
 import { formatMoney } from "../../utils/money";
+import axios from "axios";
 
 
-export function ProductsGrid({ products }) {
+export function ProductsGrid({ products, loadCart }) {
   return (
     <div className="products-grid">
       {products.map((product) => {
@@ -50,7 +51,14 @@ export function ProductsGrid({ products }) {
               Added
             </div>
 
-            <button className="add-to-cart-button button-primary">
+            <button className="add-to-cart-button button-primary"
+                onClick={async () => {
+                  await axios.post('/api/cart-items', { //updating the data ".post()"
+                    productId: product.id,
+                    quantity: 1
+                  });
+                  await loadCart();
+                }} >
               Add to Cart
             </button>
           </div>
